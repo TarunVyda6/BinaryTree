643b35479aadf111c3cd490cbe2c21d1279472bc
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-29 10:22:46 +0530

    Added : Feature to check size of all added elements

--- MyBinaryTree/src/test/java/MyBinaryTree/MyBinaryTreeTest.java
+++ MyBinaryTree/src/test/java/MyBinaryTree/MyBinaryTreeTest.java
@@ -6,12 +6,22 @@ import org.junit.Test;
 public class MyBinaryTreeTest {
 
 	@Test
-	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+	public void given13NumbersToAdd_WhenAnlysed_ShouldReturnSize13() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
-		Assert.assertEquals(3, myBinaryTree.size());
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		Assert.assertEquals(13, myBinaryTree.size());
 	}
 
 }
